9312b2fe01877c5421af186263ee2a97eba384af
Basel-AlDwairi <aldwairi.basel0@gmail.com>
2025-10-20 13:43:53 +0300

    citycenter scrap

---
+++ src/city_center_scrap.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+
+# reg_price = 'https://citycenter.jo/computer-hardware/components-cpu-and-processor/amd-ryzen-9-9900x3d-12-core-4-4ghz-5-5-ghz-max-boost-140mb-cache-am5-desktop-processor-tray'
+# old_new_price = 'https://citycenter.jo/computer-hardware/components-cpu-and-processor/amd-ryzen-9-9950x3d-16-core-4-3ghz-5-7-ghz-max-boost-132mb-cache-am5-desktop-processor-tray'
+#
+
+def scrap_citycenter(link):
+    page_text = requests.get(link).text
+    soup = BeautifulSoup(page_text,parser='lxml',features='lxml')
+
+    with open('../testing_files/cpu_html_page.txt', 'w', encoding='utf-8') as f:
+        f.write(soup.prettify())
+
+    product_name = soup.find('div', class_='tb_wt tb_wt_page_title_system tb_mb_10 display-block tb_system_page_title').text
+
+    product_description =  soup.find('ul', class_='a-unordered-list a-vertical a-spacing-mini')
+
+    product_description_items = product_description.find_all('li', class_='a-spacing-mini')
+
+    product_description_list = []
+    for item in product_description_items:
+        product_description_list.append(item.text)
+    product_info = soup.find('div', class_='tb_wt tb_wt_product_info_system display-inline-block tb_system_product_info')
+
+    product_info_dt = product_info.find_all('dt')
+
+    product_info_dt = [item.text[:-1] for item in product_info_dt]
+
+    product_info_dd = product_info.find_all('dd')
+
+    product_info_dd = [item.text for item in product_info_dd]
+
+    product_info = zip(product_info_dt,product_info_dd)
+
+    price_tag = soup.find('div', class_='price')
+
+    reg = price_tag.find('span', class_='price-regular')
+
+    def get_price(price_list):
+        price_list = price_list.replace('\n', '').split()
+        currency = price_list[0]
+        price = price_list[1]
+        return currency, price
+
+    if reg:
+        currency, price = get_price(reg.text)
+        sale = None
+    else:
+        price = price_tag.find('span', class_='price-old')
+        sale = price_tag.find('span', class_='price-new')
+        currency, price = get_price(price.text)
+        currency, sale = get_price(sale.text)
+
+    whatsapp_button = soup.find('a', id= 'whatsapp-btn-pro')
+    whatsapp_number =  whatsapp_button.get_attribute_list('href')[0][-12:]
+    whatsapp_number = ''.join(['+',whatsapp_number])
+
+
+    return link, product_name, product_description_list, currency, price, sale, whatsapp_number
+
---
+++ src/testing.py
@@ -0,0 +1,3 @@
+from src.city_center_scrap import scrap_citycenter
+
+print(scrap_citycenter('https://citycenter.jo/computer-hardware/components-memory-ram/adata-xpg-spectrix-d35g-8gb-rgb-ddr4-desktop-memory-3600-mhz-clock-speed-cl18-latency-supports-intel-xmp-2-0-1-35v-rohs-compliant-white'))
